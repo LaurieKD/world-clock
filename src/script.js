16ea75ea0b4c2cac8d87fedb372eb47cf1538e43
LaurieKD <124356856+LaurieKD@users.noreply.github.com>
2023-03-08 14:05:40 +0100

    Added JS to connect dropdown with cities
    The website now shows the local time and date for any city chosen in the dropdown.

--- src/script.js
+++ src/script.js
@@ -1,23 +1,54 @@
 function updateTime() {
 	// Los Angeles
 	let losAngelesElement = document.querySelector("#los-angeles");
-	let losAngelesDateElement = losAngelesElement.querySelector(".date");
-	let losAngelesTimeElement = losAngelesElement.querySelector(".time-element");
-	let losAngelesAmPmElement = losAngelesElement.querySelector(".am-pm");
+	if (losAngelesElement) {
+		let losAngelesDateElement = losAngelesElement.querySelector(".date");
+		let losAngelesTimeElement = losAngelesElement.querySelector(".time-element");
+		let losAngelesAmPmElement = losAngelesElement.querySelector(".am-pm");
 
-	losAngelesDateElement.innerHTML = moment().format("MMMM Do, YYYY");
-	losAngelesTimeElement.innerHTML = moment().tz("America/Los_Angeles").format("HH:mm:ss");
-	losAngelesAmPmElement.innerHTML = moment().tz("America/Los_Angeles").format("A");
+		losAngelesDateElement.innerHTML = moment().format("MMMM Do, YYYY");
+		losAngelesTimeElement.innerHTML = moment().tz("America/Los_Angeles").format("HH:mm:ss");
+		losAngelesAmPmElement.innerHTML = moment().tz("America/Los_Angeles").format("A");
 
-	// Sydney
-	let sydneyElement = document.querySelector("#sydney");
-	let sydneyDateElement = sydneyElement.querySelector(".date");
-	let sydneyTimeElement = sydneyElement.querySelector(".time-element");
-	let sydneyAmPmElement = sydneyElement.querySelector(".am-pm");
+		// Sydney
+		let sydneyElement = document.querySelector("#sydney");
+		let sydneyDateElement = sydneyElement.querySelector(".date");
+		let sydneyTimeElement = sydneyElement.querySelector(".time-element");
+		let sydneyAmPmElement = sydneyElement.querySelector(".am-pm");
 
-	sydneyDateElement.innerHTML = moment().format("MMMM Do, YYYY");
-	sydneyTimeElement.innerHTML = moment().tz("Australia/Sydney").format("HH:mm:ss");
-	sydneyAmPmElement.innerHTML = moment().tz("Australia/Sydney").format("A");
+		sydneyDateElement.innerHTML = moment().format("MMMM Do, YYYY");
+		sydneyTimeElement.innerHTML = moment().tz("Australia/Sydney").format("HH:mm:ss");
+		sydneyAmPmElement.innerHTML = moment().tz("Australia/Sydney").format("A");
+	}
+}
+
+function updateCity(event) {
+	let cityTimeZone = event.target.value;
+	let city = cityTimeZone.replace("_", " ").split("/")[1];
+	let cityTime = moment().tz(`${cityTimeZone}`).format("HH:mm:ss");
+	let cityDate = moment().tz(`${cityTimeZone}`).format("MMMM Do, YYYY");
+	let cityAmPmElement = moment().tz(`${cityTimeZone}`).format("A");
+
+	let citiesElement = document.querySelector("#cities");
+
+	citiesElement.innerHTML = `
+  <div
+						class="row justify-content-between"
+				
+					>
+						<div class="city col-7">
+							<h2>${city}</h2>
+							<div class="date">${cityDate}</div>
+						</div>
+						<div class="time col-5">
+							<span class="time-element">${cityTime}</span><small> <span class="am-pm">${cityAmPmElement}</span></small>
+						</div>
+					</div>
+  
+  `;
 }
 
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
